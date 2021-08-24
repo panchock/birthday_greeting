315856d38ac7cb52e5c8b586db68a07ff80532b5
root <root@localhost.localdomain>
2021-08-24 10:36:45 -0700

    manual ack

--- birthday_greeting_worker/worker.py
+++ birthday_greeting_worker/worker.py
@@ -8,12 +8,13 @@ channel = conn.channel()
 channel.queue_declare(queue='greetings')
 
 
-def callback(cg, method, properties, body):
+def callback(ch, method, properties, body):
     print('creating greeting for {}'.format(body))
     time.sleep(10)
     print('created greeting for {}'.format(body))
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
 
-channel.basic_consume(queue='greetings', auto_ack=True, on_message_callback=callback)
+channel.basic_consume(queue='greetings', on_message_callback=callback)
 
 channel.start_consuming()
